3e1c3c8385faa7fb793a1d22d61bb5fd153830e3
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2018-04-08 14:34:47 +0200

    debut

--- liste.c
+++ liste.c
@@ -1,1 +1,36 @@
 #include "liste.h"
+
+OrderedSet initOrderedSet()
+{
+
+}
+
+void freeOrderedSet(OrderedSet os)
+{
+
+}
+
+int getNumberElt(OrderedSet os)
+{
+
+}
+
+void insertValue(OrderedSet os, int element)
+{
+
+}
+
+bool contains(OrderedSet os, int element)
+{
+
+}
+
+void printOrderedSet(OrderedSet os)
+{
+
+}
+
+OrderedSet intersect(OrderedSet os1, OrderedSet os2)
+{
+
+}
