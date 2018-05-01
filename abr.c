72e1b5c633e1386a3352ebe4420c8abbd82ef885
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2018-05-01 15:13:24 +0200

    debug

--- abr.c
+++ abr.c
@@ -135,7 +135,6 @@ void printBinarySearchTreeAux(SearchTree st, int niveau)
 
     printf("%*s%s ", niveau, "", st->mot);
     printOrderedSet(st->positions);
-    printf("\n");
 
     printBinarySearchTreeAux(st->fg, niveau + 1);
     printBinarySearchTreeAux(st->fd, niveau + 1);
